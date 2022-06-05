f3f7639b39ec05781715e7d6a30a04fd35929351
gabelopes <gabriellopes.nunes@hotmail.com>
2022-06-05 20:29:01 -0300

    Creates TagLabel component

---
+++ src/components/TagLabel.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>
+    <slot />
+  </span>
+</template>
+
+<script>
+  export default {
+    name: "TagLabel"
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "@/styles/variables/_colors.scss";
+  @import "@/styles/variables/_fonts.scss";
+
+  .tag-label {
+    color: $light-gray;
+    font-family: "Arvo", serif;
+    font-weight: $bold-weight;
+    text-transform: uppercase;
+  }
+</style>
