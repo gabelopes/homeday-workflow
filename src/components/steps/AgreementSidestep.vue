2b34678771210436e7e9e0fb9e971a805ba74312
gabelopes <gabriellopes.nunes@hotmail.com>
2022-06-05 20:30:11 -0300

    Creates sidesteps

---
+++ src/components/steps/AgreementSidestep.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="agreement-sidestep">
+    <Icon class="agreement-sidestep__icon" name="mail" variant="side" />
+
+    <div class="agreement-sidestep__copy">
+      <TagLabel class="tag-label">{{ $t("agreementSidestep.intro") }}</TagLabel>
+
+      <h2 class="agreement-sidestep__title">
+        {{ $t("agreementSidestep.title") }}
+      </h2>
+
+      <p class="agreement-sidestep__description">
+        {{ $t("agreementSidestep.description") }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Icon from "@/components/Icon.vue";
+  import TagLabel from "@/components/TagLabel.vue";
+  import { mapActions } from "vuex";
+
+  export default {
+    name: "AgreementSidestep",
+    components: {
+      Icon,
+      TagLabel
+    },
+    methods: {
+      ...mapActions(["setTest"])
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "@/styles/mixins/_breakpoints.scss";
+  @import "@/styles/mixins/_layout.scss";
+  @import "@/styles/variables/_colors.scss";
+
+  .agreement-sidestep {
+    @include flex-box();
+
+    @include tablet() {
+      @include flex-box(center, center, column);
+    }
+
+    padding: 30px;
+
+    @include tablet() {
+      padding: 40px;
+    }
+
+    @include desktop() {
+      padding-left: 50px;
+    }
+
+    &__icon {
+      @include mobile() {
+        margin-right: 20px;
+      }
+
+      @include tablet() {
+        margin-bottom: 50px;
+      }
+    }
+
+    &__description {
+      color: $darkest-gray;
+    }
+  }
+</style>
---
+++ src/components/steps/PersonalSidestep.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="personal-sidestep">
+    <Icon class="personal-sidestep__icon" name="user" variant="side" />
+
+    <div class="personal-sidestep__copy">
+      <TagLabel class="tag-label">{{ $t("personalSidestep.intro") }}</TagLabel>
+
+      <h2 class="personal-sidestep__title">
+        {{ $t("personalSidestep.title") }}
+      </h2>
+
+      <p class="personal-sidestep__description">
+        {{ $t("personalSidestep.description") }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Icon from "@/components/Icon.vue";
+  import TagLabel from "@/components/TagLabel.vue";
+  import { mapActions } from "vuex";
+
+  export default {
+    name: "PersonalSidestep",
+    components: {
+      Icon,
+      TagLabel
+    },
+    methods: {
+      ...mapActions(["setTest"])
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "@/styles/mixins/_breakpoints.scss";
+  @import "@/styles/mixins/_layout.scss";
+  @import "@/styles/variables/_colors.scss";
+
+  .personal-sidestep {
+    @include flex-box();
+
+    @include tablet() {
+      @include flex-box(center, center, column);
+    }
+
+    padding: 30px;
+
+    @include tablet() {
+      padding: 40px;
+    }
+
+    @include desktop() {
+      padding-left: 50px;
+    }
+
+    &__icon {
+      @include mobile() {
+        margin-right: 20px;
+      }
+
+      @include tablet() {
+        margin-bottom: 50px;
+      }
+    }
+
+    &__description {
+      color: $darkest-gray;
+    }
+  }
+</style>
