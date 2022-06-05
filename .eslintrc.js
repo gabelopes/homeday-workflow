f5c413e04214360257e10f185c0464e6b3496f97
gabelopes <gabriellopes.nunes@hotmail.com>
2022-06-05 12:28:06 -0300

    Adds initial prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,38 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "plugin:prettier/recommended",
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: "@babel/eslint-parser"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": 0,
+    "prettier/prettier": [
+      "error",
+      {
+        tabWidth: 2,
+        trailingComma: "none",
+        vueIndentScriptAndStyle: true
+      }
+    ]
   },
   overrides: [
     {
       files: [
         "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
-        jest: true,
-      },
-    },
-  ],
+        jest: true
+      }
+    }
+  ]
 };
